f7e12b08fa0bc4834ad71bf7336d3f2d8feef9e5
Yujun <u7667126@anu.edu.au>
2024-03-17 23:21:35 +1100

    modified:   package-lock.json 	modified:   package.json 	modified:   src/index.js 	modified:   src/page/Home/index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter,Router,RouterProvider } from 'react-router-dom';
+import {RouterProvider } from 'react-router-dom';
 import router from './router';
 
 
@@ -11,6 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
+    {/* <App/> */}
   </React.StrictMode>
 );
 
--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -1,14 +1,87 @@
 import { Link } from "react-router-dom";
+import FileSaver from 'file-saver';
+import Papa from "papaparse";
+import jschardet from "jschardet";
+import iconv from "iconv-lite";
+import encoding from "encoding";
+
+import React, { useState } from 'react';
+import { Button, Upload, notification } from 'antd';
+import { InboxOutlined } from '@ant-design/icons';
+
+const { Dragger } = Upload;
 
 const Home = ()=>{
+  const [nextStepVisible, setNextStepVisible] = useState(false);
+  const [importedData, setImportedData] = useState([]); // importedData to store imported json data
+
+  const parseUploadCSVData = (file, callback) => {
+    const fReader = new FileReader();
+    fReader.readAsBinaryString(file);
+    fReader.onload = (event) => {
+      let fileBuf = event.target.result;
+      const encodeType = jschardet.detect(fileBuf).encoding;
+  
+      let textData;
+      if (encodeType !== "UTF-8") {
+        textData = iconv.decode(fileBuf, encodeType);
+      } else {
+        textData = fileBuf;
+      }
+  
+      iconv.skipDecodeWarning = true;
+  
+      Papa.parse(file, {
+        encoding: textData,
+        complete: function(results) {
+          const res = results.data;
+          if (res[res.length - 1] === "") {
+            res.pop();
+          }
+  
+          // Use the first row as column labels
+          const columnLabels = res[0];
+  
+          // Convert CSV data to JSON format
+          const jsonData = res.slice(1).map(row => {
+            const obj = {};
+            row.forEach((value, index) => {
+              obj[columnLabels[index]] = value;
+            });
+            return obj;
+          });
+  
+          setImportedData(jsonData);
+          console.log('importedData updated (JSON format):', jsonData);
+        }
+      });
+    }
+  }
+    const renderNextStep = () => {
+        setNextStepVisible(true);
+      }
+    
+      const handleNextStep = () => {
+        console.log('下一步操作');
+        console.log("data",importedData);
+
+      }
+    
+      const uploadProps = {
+        name: 'file',
+        multiple: false,
+        showUploadList: false,
+        beforeUpload: (file) => {
+          parseUploadCSVData(file, renderNextStep);
+          return false;
+        },
+      };
     return(
         <div className="index">
-        <head>
         <title>Welcome Page</title>
 
         <link rel="stylesheet" href="../CSS/bar.css"/>
         <link rel="stylesheet" href="../CSS/button.css"/>
-            </head>
 
             <div>
             <ul>
@@ -28,7 +101,20 @@ const Home = ()=>{
             All-in-one dashboard for tracking student engagement
         </p>
 
-        <button class="button buttong" onclick="window.location='import.html';">Import (Nor implemented)</button>
+        {/* <button class="button buttong" onClick="window.location='import.html';">Import (Nor implemented)</button> */}
+        <div>
+        <Dragger {...uploadProps}>
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">upload csv file</p>
+      </Dragger>
+      {nextStepVisible && (
+        <Button type="primary" onClick={handleNextStep}>
+          下一步
+        </Button>
+      )}
+    </div>
   
 
 
