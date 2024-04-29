550d6eacee58332299d2de08644516346cc3bbc3
lyj0209 <694265147@qq.com>
2024-04-29 13:38:25 +1000

    add score type management, score type: quiz, attendance, exam, assignment

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import importedDataReducer from './modules/importedDataStore';
-
+import scoreTypeReducers from './modules/scoreTypeStore';
 const store = configureStore({
   reducer: {
     importedData: importedDataReducer,
+    scoreType: scoreTypeReducers
+    
   },
 });
 
