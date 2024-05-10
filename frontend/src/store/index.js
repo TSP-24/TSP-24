8e3997193fa842e874b490228a0595aa5c4a9a39
u7542145 <u7542145@anu.edu.au>
2024-05-10 17:16:35 +1000

    Refactored front and back end codes

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import importedDataReducer from './modules/importedDataStore';
-import scoreTypeReducers from './modules/scoreTypeStore.js';
+
 const store = configureStore({
   reducer: {
-    importedData: importedDataReducer,
-    scoreType: scoreTypeReducers
-    
+    importedData: importedDataReducer, 
   },
 });
 
