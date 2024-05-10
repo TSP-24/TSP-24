de7a00331371d0edf28ba110b52290f1e7ccb0c5
lyj0209 <694265147@qq.com>
2024-05-10 15:29:40 +1000

    deleting redundant scoring function in frontend

--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -2,14 +2,13 @@ import { Link } from "react-router-dom";
 import Papa from "papaparse";
 import jschardet from "jschardet";
 import iconv from "iconv-lite";
-import React, { useState } from 'react';
-import { Button, Upload } from 'antd';
-import { InboxOutlined } from '@ant-design/icons';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { Button, Upload } from "antd";
+import { InboxOutlined } from "@ant-design/icons";
+import { useSelector, useDispatch } from "react-redux";
 import { setImportedData } from "../../store/modules/importedDataStore";
-import {classifyColumnLabels} from "../../utils/scoreTypeClasscify";
+import { classifyColumnLabels } from "../../utils/scoreTypeClasscify";
 import { setScoreType } from "../../store/modules/scoreTypeStore";
-
 
 const { Dragger } = Upload;
 
@@ -26,123 +25,50 @@ const Home = () => {
     fReader.onload = (event) => {
       let fileBuf = event.target.result;
       const encodeType = jschardet.detect(fileBuf).encoding;
-  
+
       let textData;
       if (encodeType !== "UTF-8") {
         textData = iconv.decode(fileBuf, encodeType);
       } else {
         textData = fileBuf;
       }
-  
+
       iconv.skipDecodeWarning = true;
-  
+
       Papa.parse(file, {
         encoding: textData,
-        complete: function(results) {
+        complete: function (results) {
           const res = results.data;
           if (res[res.length - 1] === "") {
             res.pop();
           }
-  
-          // Use the first row as column labels
-          const columnLabels = res[0];
 
-          //classify quiz, assignment, exam, attendance
-          const classifiedLabels = classifyColumnLabels(columnLabels);
-
-          // dispatch(setScoreType(classifiedLabels.quiz));
-          // dispatch(setScoreType(classifiedLabels.exam));
-          // dispatch(setScoreType(classifiedLabels.assignment));
-          // dispatch(setScoreType(classifiedLabels.attendance));
-          dispatch(setScoreType(classifiedLabels));
-
-          console.log("classifiedLabels",classifiedLabels)
-
-          console.log("scoreType",scoreType); 
-          const quizCount = Array.isArray(classifiedLabels.quiz) ? classifiedLabels.quiz.length : 0;
-          const examCount = Array.isArray(classifiedLabels.exam) ? classifiedLabels.exam.length : 0;
-          const assignmentCount = Array.isArray(classifiedLabels.assignment) ? classifiedLabels.assignment.length : 0;
-          const attendanceCount = Array.isArray(classifiedLabels.attendance) ? classifiedLabels.attendance.length : 0;
-
-          //Here we suppose a fixed weight for each category: total quiz-10%, total assignment-40%, total exam-40%, attendance-10%.
-          //And in each category, we give even weights.
-          //If one category of above not exists, we do not count it in totalWeight, and calculate others by same proportion.
-          
-          let totalWeight = 0;
-          if(quizCount!==0){
-            totalWeight+=0.1
-          }
-          if(examCount!==0){
-            totalWeight+=0.4
-          }
-          if(assignmentCount!==0){
-            totalWeight+=0.4
-          }
-          if(attendanceCount!==0){
-            totalWeight+=0.1
-          }
-
-         
           // Convert CSV data to JSON format
-          const jsonData = res.slice(1).map(row => {
+          const jsonData = res.slice(1).map((row) => {
             const obj = {};
             row.forEach((value, index) => {
               obj[columnLabels[index]] = value;
             });
             return obj;
           });
-  
+
           dispatch(setImportedData(jsonData));
-          console.log('importedData updated (JSON format):', importedData);
-
-          //For every data column in category above, get the highest data in the column as the full mark.
-          // 
-
-      const quizScores = classifiedLabels.quiz;
-      console.log("quizScores.type",quizScores.type)
-
-      
-      const maxScores = {};
-
-      for (const quiz of quizScores) {
-      let maxScore = -0.1;
-
-      for (const data of importedData) {
-        const quizName = "Quiz: "+quiz; 
-        if (data.hasOwnProperty(quizName)) { 
-        const value = Number(data[quizName]);
-          if (value>maxScore){
-            maxScore = value;
-      }
-  }
-  }
-
-  maxScores[quiz] = maxScore; 
-}
-
-console.log("maxScores of quizs:", maxScores);
-
-
-
-
-    }});
-
-      
-
-
-    }
-  }
+          console.log("importedData updated (JSON format):", importedData);
+        },
+      });
+    };
+  };
 
   const renderNextStep = () => {
     setNextStepVisible(true);
-  }
+  };
 
   const handleNextStep = () => {
     console.log(importedData);
-  }
+  };
 
   const uploadProps = {
-    name: 'file',
+    name: "file",
     multiple: false,
     showUploadList: false,
     beforeUpload: (file) => {
@@ -158,16 +84,23 @@ console.log("maxScores of quizs:", maxScores);
       <link rel="stylesheet" href="../CSS/button.css" />
       <div>
         <ul>
-          <li><Link to="/search">Search</Link></li>
-          <li><Link to="/students">Students</Link></li>
-          <li><Link to="/">Home</Link></li>
+          <li>
+            <Link to="/search">Search</Link>
+          </li>
+          <li>
+            <Link to="/students">Students</Link>
+          </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
         </ul>
       </div>
       <h1>Welcome!</h1>
       <br />
       <br />
       <p>
-        Student progress tracker.<br />
+        Student progress tracker.
+        <br />
         All-in-one dashboard for tracking student engagement
       </p>
       <div>
@@ -184,8 +117,7 @@ console.log("maxScores of quizs:", maxScores);
         )}
       </div>
     </div>
-
-  )
-}
+  );
+};
 
 export default Home;
