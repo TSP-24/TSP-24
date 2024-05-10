62db8c395373c37185c12f8e933cd30c3fa5fc0d
u7542145 <u7542145@anu.edu.au>
2024-05-10 10:40:20 +1000

    Added ability to sort each column in the students table

--- frontend/src/page/Students/index.js
+++ frontend/src/page/Students/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Select } from 'antd';
+import { Link } from "react-router-dom";
 import { useSelector } from 'react-redux';
 
 
 
-const Students = () => {
+const { Option } = Select;
+
+
+const Students = ({}) => {
+  const [sortColumn, setSortColumn] = useState(null);
+  const [sortOrder, setSortOrder] = useState('asc');
+  const [filters, setFilters] = useState({});
+
   const importedData = useSelector((state) => state.importedData);
 
   const parsedData = importedData;
@@ -18,6 +26,90 @@ const Students = () => {
     return 'black'; // Default color
   };
 
+  const handleSort = (column, order) => {
+    setSortColumn(column);
+    setSortOrder(order);
+  };
+
+  const handleFilterChange = (column, values) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [column]: values,
+    }));
+  };
+
+  const filteredData = parsedData.filter((student) =>
+  Object.entries(filters).every(
+    ([column, filterValues]) =>
+      filterValues.length === 0 || filterValues.includes('All') || filterValues.includes(student[column])
+  )
+);
+
+  const sortedData = filteredData.sort((a, b) => {
+    if (sortColumn) {
+      const order = sortOrder === 'asc' ? 1 : -1;
+      return (a[sortColumn] - b[sortColumn]) * order;
+    }
+    return b.Engagement - a.Engagement;
+    
+  });
+
+  const exportToCSV = () => {
+    const csvData = sortedData.map((student) =>
+      columns.map((column) => student[column]).join(',')
+    ).join('\n');
+
+    const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8' });
+    const link = document.createElement('a');
+    const fileName = 'table_data.csv';
+
+    if (link.download !== undefined) {
+      link.setAttribute('href', URL.createObjectURL(blob));
+      link.setAttribute('download', fileName);
+      link.style.visibility = 'hidden';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } else {
+      window.open(URL.createObjectURL(blob));
+    }
+  };
+
+
+  const renderColumnHeader = (column) => {
+    const filterValues = filters[column] || [];
+    const options = Array.from(new Set(parsedData.map((student) => student[column]))).map(
+      (value) => (
+        <Option key={value} value={value}>
+          {value}
+        </Option>
+      )
+    );
+  
+    return (
+      <th key={column}>
+        {column}
+        <Select
+          mode="multiple"
+          value={filterValues.includes('All') ? ['All'] : filterValues}
+          onChange={(values) => handleFilterChange(column, values)}
+          style={{ width: '200px' }}
+          dropdownRender={(menu) => (
+            <div>
+              {menu}
+              <div className="sort-buttons">
+                <button onClick={() => handleSort(column, 'asc')}>Sort Ascending</button>
+                <button onClick={() => handleSort(column, 'desc')}>Sort Descending</button>
+              </div>
+            </div>
+          )}
+        >
+          <Option value="All">All</Option>
+          {options}
+        </Select>
+      </th>
+    );
+  };
   return (
     <div className="App">
       <ul>{/* {channelList.map(item=> <li key={item.id}>{item.name}</li>)} */}</ul>
@@ -35,74 +127,67 @@ const Students = () => {
         </ul>
       </div>
       <h1>Student</h1>
+
+      <br></br>
+
       <div className="table_background">
-        <table id="main_table">
-          <thead>
-            <tr>
-              {columns.map((column, index) => (
-                <th key={index}>{column}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-          {parsedData.map((student, index) => (
-            <tr key={index}>
-              {columns.map((column, columnIndex) => {
-                // Check if the current column is 'Info'
-                if (column === 'Info') {
-                  return (
-                    // If it is 'Info', we render a cell with styled divs for each piece of info
-                    <td key={columnIndex}>
-                      {student[column].map((info, infoIndex) => (
-                        <div key={infoIndex} style={{ color: getColorByInfo(info[0]) }}>
-                          {info[0]}: {info[1]}
-                        </div>
-                      ))}
-                    </td>
-                  );
-                } 
-                else if(column === 'Failed Assessments') {
-                  return (
-                    // If it is "Failed assessments", we colour the assessments name to blue, and leave the mark red
-                    <td key={columnIndex}>
-                      {student[column].map((assi, assiIndex) => (
-                        <div key={assiIndex}>
-                          <span style={{ color: 'blue'}}>{assi[0]}</span>
-                          {/* Apply a different style for info[1] */}
-                          <span style={{ color: 'red'}}> {assi[1]}</span>
-                        </div>
-                      ))}
-                    </td>
-                  )
-                }
-                else if(column === 'Late Submissions') {
-                  return (
-                    // If it is "Late submissions", we colour the assessments name to blue, and leave the mark red
-                    <td key={columnIndex}>
-                      {student[column].map((assi, assiIndex) => (
-                        <div key={assiIndex}>
-                          <span style={{ color: 'blue'}}>{assi[0]}</span>
-                          {/* Apply a different style for info[1] */}
-                          <span style={{ color: 'red'}}> {assi[1]}</span>
-                        </div>
-                      ))}
-                    </td>
-                  )
-                } 
-                else {
-                  // For all other columns, we render the data as plain text
-                  return <td key={columnIndex}>{student[column]}</td>;
-                }
-              })}
-            </tr>
-          ))}
-        </tbody>
-        </table>
-      </div>
-      <br />
-      <button className="button buttong" onClick={() => window.location = 'import.html'}>
-        Export (Not implemented)
+      <table id="main_table">
+        <thead>
+          <tr>{columns.map(renderColumnHeader)}</tr>
+        </thead>
+        <tbody>
+      {sortedData.map((student, index) => (
+        <tr key={index}>
+          {columns.map((column, columnIndex) => {
+            if (column === 'Info') {
+              return (
+                <td key={columnIndex}>
+                  {student[column].map((info, infoIndex) => (
+                    <div key={infoIndex} style={{ color: getColorByInfo(info[0]) }}>
+                      {info[0]}: {info[1]}
+                    </div>
+                  ))}
+                </td>
+              );
+            } else if (column === 'Failed Assessments') {
+              return (
+                <td key={columnIndex}>
+                  {student[column].map((assi, assiIndex) => (
+                    <div key={assiIndex}>
+                      <span style={{ color: 'blue' }}>{assi[0]}</span>{' '}
+                      <span style={{ color: 'red' }}>{assi[1]}</span>
+                    </div>
+                  ))}
+                </td>
+              );
+            } else if (column === 'Late Submissions') {
+              return (
+                <td key={columnIndex}>
+                  {student[column].map((assi, assiIndex) => (
+                    <div key={assiIndex}>
+                      <span style={{ color: 'blue' }}>{assi[0]}</span>{' '}
+                      <span style={{ color: 'red' }}>{assi[1]}</span>
+                    </div>
+                  ))}
+                </td>
+              );
+            } else {
+              return <td key={columnIndex}>{student[column]}</td>;
+            }
+          })}
+        </tr>
+      ))}
+    </tbody>
+      </table>
+    </div>
+
+
+    
+    <br />
+    <button className="button buttong" onClick={exportToCSV}>
+        Export (currently filtered table)
       </button>
+      
     </div>
   );
 };
